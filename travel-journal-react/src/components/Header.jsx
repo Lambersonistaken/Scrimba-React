a37647519759fb9fc5902150f7cf2b0c94e48ebb
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-09-22 20:19:25 +0300

    header component done

---
+++ travel-journal-react/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+import Globe from "../assets/Globe.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <img src={Globe} alt="" />
+        <h2>my travel journal.</h2>
+      </header>
+    </div>
+  )
+}
+
+export default Header
