acae7a169738dd7995dce71aeaa5128e72e0c595
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-13 20:06:26 +0300

    van details page ready

--- van-life/src/pages/VanDetails.jsx
+++ van-life/src/pages/VanDetails.jsx
@@ -2,10 +2,22 @@ import Arrow from "../assets/arrow.svg"
 import { Link } from "react-router-dom"
 import "./VanDetails.css"
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const VanDetails = () => {
   const params = useParams();
-  console.log(params)
+
+  const [vanDetail, setVanDetail] = useState();
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+    .then(res => res.json())
+    .then(data => {
+        setVanDetail(data.vans)
+    })
+  }, [])
+
+  console.log(vanDetail)
 
   return (
     <div className="van-detail-page">
@@ -13,6 +25,16 @@ const VanDetails = () => {
         <img className="arrow-icon" src={Arrow} alt="" />
         <Link to="/vans">Back to all vans</Link>
       </div>
+
+      <div className="van-detail-container">
+        <img className="van-image" src={vanDetail.imageUrl} alt="" />
+        <div>{vanDetail.type}</div>
+        <h1>{vanDetail.name}</h1>
+        <h3>{vanDetail.price}</h3>
+        <p>{vanDetail.description}</p>
+        <button>Rent this van</button>
+      </div>
+
     </div>
   )
 }
