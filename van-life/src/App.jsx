c3810aad48e62a61f7f79c6f0ee3ddfbcda57cfd
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-13 18:13:54 +0300

    mirage server added to project

--- van-life/src/App.jsx
+++ van-life/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Header from './components/Header'
 import Home from "./pages/Home"
 import About from "./pages/About"
+import "./server"
 
 function App() {
   
