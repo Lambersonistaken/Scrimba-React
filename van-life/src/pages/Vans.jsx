c73bb9c588f2045ffab4cb8419235e0fead02622
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-13 18:43:58 +0300

    vans fetched and added to screen

--- van-life/src/pages/Vans.jsx
+++ van-life/src/pages/Vans.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react"
+import "./Vans.css"
 
 
 export default function Vans() {
+
+    const [vanData,setVanData] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/vans")
+        .then(res => res.json())
+        .then(data => {
+            setVanData(data.vans)
+        })
+    }, [])
+
+    const vans = vanData.map((item) => {
+        return (
+            <div key={item.id} className="van-card">
+                <img className="van-img" src={item.imageUrl} alt="" />
+                <div className="title-con">
+                <h4>{item.name}</h4>
+                <p>{item.price}/$</p>
+                </div>
+                <div className="type-con">
+                    <p>{item.type}</p>
+                </div>
+                
+            </div>
+        )
+    })
+    
+
+
     return (
-        <div className="vans-container">
-            <h1>Vans goes here</h1>
+        <div className="vans-page">
+            <div className="van-container">
+            {vans}
+            </div>
             
         </div>
     )
