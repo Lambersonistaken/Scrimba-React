7c8e5622eabe8b87a48dc27fdc086309af30cb7d
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-19 15:17:08 +0300

    layout route added

--- van-life/src/App.jsx
+++ van-life/src/App.jsx
@@ -1,22 +1,23 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Header from './components/Header'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetails from "./pages/VanDetails"
 import "./server"
+import Layout from './components/Layout'
 
 function App() {
   
   return (
     <BrowserRouter>
-    <Header/>
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/vans" element={<Vans/>}/>
-    <Route path="/vans/:id" element={<VanDetails/>}/>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans/>}/>
+        <Route path="/vans/:id" element={<VanDetails/>}/>
+      </Route>
     </Routes>
     </BrowserRouter>
   )
