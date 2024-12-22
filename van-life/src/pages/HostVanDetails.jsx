d0d89f7951afaf93f8c11c345aa272d507b28e9e
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-22 18:46:27 +0300

    update

--- van-life/src/pages/HostVanDetails.jsx
+++ van-life/src/pages/HostVanDetails.jsx
@@ -1,9 +1,32 @@
-
+import { Link, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const HostVanDetails = () => {
+
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = useState(null)
+
+    useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans[0]))
+    }, [])
+
+    
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+    
+
   return (
     <div>
-        <h1>Host van details will be here</h1>
+        <Link to="/host/vans">Back to all vans</Link>
+        <div>
+            <img src={currentVan.imageUrl} width={150} />
+            <h2>{currentVan.name}</h2>
+            <p>{currentVan.price}</p>
+            <p>{currentVan.type}</p>
+        </div>
     </div>
   )
 }
